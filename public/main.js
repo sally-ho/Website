a46d9097507c13f0c5b22fe4f63cf8cd998e714f
Sally Ho <sallyho123@yahoo.com>
2020-12-04 05:36:29 -0800

    moved the newsfeed button

--- public/main.js
+++ public/main.js
@@ -22,27 +22,6 @@ function loadHuntContent(content) {
   $("#hunt_content").html(content);
 }
 
-function requestNewsfeedData (){
-  $.ajax({
-    dataType: "json",
-    url: "https://firebasestorage.googleapis.com/v0/b/advancedtopicscs.appspot.com/o/blogArticles.json?alt=media&token=478d06a6-d7e5-4cac-a92d-7670dea8de17",
-    success: loadNewsfeedJSONAsHTML,
-  });
-}
-
-function loadNewsfeedJSONAsHTML(result) {
-  console.log(result);
-  var myString = "";
-  for (var i = 0; i < result.length; i++) {
-    var nextResult = result[i];
-
-    myString += "<h2>" + nextResult['type'] + "</h2>";
-  }
-  // Set the HTML
-  $('.newsfeed').html(myString);
-}
-
 $("#load_home").click(requestContact);
 $("#game_button").click(loadGame);
 $("#search_button").click(requestHuntContent);
-$("#step5_button").click(requestNewsfeedData);
--- public/step5.js
+++ public/step5.js
@@ -1,21 +1,23 @@
-function requestNewsfeedData (){
-  $.ajax({
-    dataType: "json",
-    url: "https://firebasestorage.googleapis.com/v0/b/advancedtopicscs.appspot.com/o/blogArticles.json?alt=media&token=478d06a6-d7e5-4cac-a92d-7670dea8de17",
-    success: loadNewsfeedJSONAsHTML,
-  });
-}
-
 function loadNewsfeedJSONAsHTML(result) {
-  console.log(result);
-  var myString = "";
+  var newsfeed = "";
   for (var i = 0; i < result.length; i++) {
     var nextResult = result[i];
 
-    myString += "<h2>" + nextResult['type'] + "</h2>";
+    newsfeed += "<h2>" + nextResult['headline'] + "</h2>" + "<p>" + nextResult['author'] + "</p>";
   }
-  // Set the HTML
-  $('.newsfeed').html(myString);
+
+  $("#newsfeed_content").html(newsfeed);
+
 }
 
-requestNewsfeedData ();
+function requestNewsFeed() {
+  $.ajax({
+    dataType: "json",
+    url: "https://firebasestorage.googleapis.com/v0/b/advancedtopicscs.appspot.com/o/blogArticles.json?alt=media&token=478d06a6-d7e5-4cac-a92d-7670dea8de17",
+    success : loadNewsfeedJSONAsHTML,
+  });
+}
+
+$("#news_button").click(function() {
+  requestNewsFeed();
+})
